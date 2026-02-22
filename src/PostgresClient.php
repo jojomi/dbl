d18e78d3b35bfc92c30b66dadcb81cb6c9bcf8bd
jojomi <dev.jojomi@yahoo.com>
2026-02-22 06:07:14 +0100

    Add PostgresClient

---
+++ src/PostgresClient.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Jojomi\Dbl;
+
+use PDO;
+use PDOException;
+use RuntimeException;
+use function getenv;
+use function sprintf;
+
+final class PostgresClient extends BasicSqlClient
+{
+    public function __construct(
+        private readonly PostgresCredentials $credentials,
+        private readonly ?int $port = null,
+    ) {}
+
+    public static function fromEnv(): self
+    {
+        $port = getenv('POSTGRES_PORT');
+
+        return new self(
+            PostgresCredentials::fromEnv(),
+            $port === false ? null : (int) $port
+        );
+    }
+
+    public function getConnection(): PDO
+    {
+        if ($this->connection !== null) {
+            return $this->connection;
+        }
+
+        $dsn = sprintf(
+            'pgsql:host=%s;port=%d;dbname=%s',
+            $this->credentials->hostname,
+            $this->port ?? 5432,
+            $this->credentials->database,
+        );
+
+        try {
+            $this->connection = new PDO(
+                $dsn,
+                $this->credentials->username,
+                $this->credentials->password,
+                [
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                ],
+            );
+        } catch (PDOException $e) {
+            throw new RuntimeException('Could not connect to database: ' . $e->getMessage(), previous: $e);
+        }
+
+        return $this->connection;
+    }
+}
---
+++ src/PostgresCredentials.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Jojomi\Dbl;
+
+use function getenv;
+
+/**
+ * PostgresCredentials.
+ */
+final readonly class PostgresCredentials
+{
+
+    private function __construct(public string $hostname, public string $username, public string $password, public string $database)
+    {
+        // NOOP
+    }
+
+    public static function create(string $hostname, string $username, string $password, string $database): self
+    {
+        return new self($hostname, $username, $password, $database);
+    }
+
+    public static function fromEnv(): self
+    {
+        $hostname = getenv('POSTGRES_HOSTNAME');
+        if ($hostname === false || $hostname === '') {
+            $hostname = '127.0.0.1';
+        }
+
+        $user = getenv('POSTGRES_USER');
+        if ($user === false || $user === '') {
+            $user = '';
+        }
+
+        $password = getenv('POSTGRES_PASSWORD');
+        if ($password === false || $password === '') {
+            $password = '';
+        }
+
+        $database = getenv('POSTGRES_DATABASE');
+        if ($database === false || $database === '') {
+            $database = '';
+        }
+
+        return self::create($hostname, $user, $password, $database);
+    }
+
+}
